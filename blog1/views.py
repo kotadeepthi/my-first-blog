e61080b882af84fa278182b42772d97b97783ac8
deepthi <kotadeepthi559@gmail.com>
2016-02-16 14:12:36 -0600

    Added templates and css and post level urls configured

--- blog1/views.py
+++ blog1/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render,get_object_or_404
+from .models import Post
 
-# Create your views here.
 def post_list(request):
-    return render(request,'blog1/post_list.html',{})
+    posts = Post.objects.all()
+    return render(request, 'blog1/post_list.html', {'posts':posts})
+def post_detail(request,pk):
+   # post=Post.objects.get(pk=pk)
+    post=get_object_or_404(Post,pk=pk)    
+    return render(request,'blog1/post_detail.html',{'post':post})
